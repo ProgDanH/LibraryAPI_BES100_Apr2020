bbeba00c851c6fa04ac3daa0d31d6d93b4b32c92
Dan Hannaway <daniel_g_hannaway@progressive.com>
2020-04-27 21:50:13 +0000

    First check-in of 100 Project

---
+++ LibraryAPI/Controllers/StatusController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    public class StatusController : Controller
+    {
+        [HttpGet("/status")]
+        public ActionResult GetStatus()
+        {
+            StatusResponse sr = new StatusResponse
+            {
+                Msg = "Send a message basic text",
+                CheckedBy = "Joe Blowmedown",
+                LastChecked = DateTime.Now
+            };
+            return Ok(sr);
+        }
+    }
+    public class StatusResponse
+    {
+        public string Msg { get; set; }
+        public string CheckedBy { get; set; }
+        public DateTime LastChecked { get; set; }
+    }
+}
